9316e0436f339118326aa0aa14828bfb497d4bbf
Thomas Briand <tom.briiand@gmail.com>
2024-01-06 19:43:41 +0800

    Trouble + Remaps + HarpoonV2

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,7 +3,24 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  {'ThePrimeagen/harpoon'},
-  {'tpope/vim-surround'},
-  {'github/copilot.vim'}
+  { 'ThePrimeagen/harpoon', branch = "harpoon2", dependencies = { { "nvim-lua/plenary.nvim" } } },
+  { 'tpope/vim-surround' },
+  { 'github/copilot.vim' },
+  {
+    'voldikss/vim-floaterm'
+  },
+  {
+    'folke/trouble.nvim',
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function ()
+      require("trouble").setup {
+        auto_open = false,
+        auto_close = true,
+      }
+      vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end, {desc="Trouble Toggle"})
+      vim.keymap.set("n", "<leader>xj", function() require("trouble").next({ skip_groups = true, jump = true }) end, {desc="Trouble Next"})
+      vim.keymap.set("n", "<leader>xk", function() require("trouble").previous({ skip_groups = true, jump = true }) end, {desc="Trouble Previous"})
+    end
+  }
+
 }
