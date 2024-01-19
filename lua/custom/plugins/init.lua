626abe89dc156ed9dbc11c6f08e59c961e7bf076
Thomas Briand <thomasbriand@SIAQ00454MB.local>
2024-01-19 15:38:39 +0800

    Telescope tabs

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -167,4 +167,15 @@ return {
       }
     end,
   },
+  {
+    'LukasPietzschmann/telescope-tabs',
+    config = function()
+      require('telescope').load_extension 'telescope-tabs'
+      require('telescope-tabs').setup {}
+      vim.keymap.set('n', '<leader>st', function()
+        require('telescope-tabs').list_tabs()
+      end, { desc = 'Telescope list tabs' })
+    end,
+    dependencies = { 'nvim-telescope/telescope.nvim' },
+  },
 }
